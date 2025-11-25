eea9ef8de3619f21dc80411b95cd155763b775ff
MarkSA81 <markamoras@gmail.com>
2025-11-25 16:01:44 -0300

    MILOTIC BUFF

--- src/data/pokemon/species_info/gen_3_families.h
+++ src/data/pokemon/species_info/gen_3_families.h
@@ -8940,7 +8940,7 @@ const struct SpeciesInfo gSpeciesInfoGen3[] =
         .growthRate = GROWTH_ERRATIC,
         .eggGroups = MON_EGG_GROUPS(EGG_GROUP_WATER_1, EGG_GROUP_DRAGON),
     #if P_UPDATED_ABILITIES >= GEN_4
-        .abilities = { ABILITY_MARVEL_SCALE, ABILITY_COMPETITIVE, ABILITY_CUTE_CHARM },
+        .abilities = { ABILITY_MARVEL_SCALE, ABILITY_COMPETITIVE, ABILITY_SERENE_GRACE },
     #else
         .abilities = { ABILITY_MARVEL_SCALE, ABILITY_NONE, ABILITY_CUTE_CHARM },
     #endif
